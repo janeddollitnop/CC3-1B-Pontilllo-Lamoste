af7a91af0a85e24f3837d239f96c8ecec0b5a4ac
janeddollitnop <45084983+janeddollitnop@users.noreply.github.com>
2019-02-20 15:11:34 +0800

    Update Character.java

--- RPG,Dice,Cha,Test/Character.java
+++ RPG,Dice,Cha,Test/Character.java
@@ -1,84 +1,72 @@
+package rpg;
 
-package rpg2;
-
-
-public class Character {
-    static Dice dice = new Dice();
-    private String name;
-    private int strength;
-    private int dexterity;
-    private int intelligence;
-    private int currentLife;
-    private int maxLife;
+/**
+ *
+ * @author m304user
+ */
+public class TestCharacter {
+    public static void main(String [] args){
+    Character p1 = new Character("Golden State Warriors", 25,55,20,40,45);
+    Wizard p2 = new Wizard("Washington Wizard", 15,50,40,20,25);
 
 
-public Character(String n, int s, int d, int i) {
- this.name=n;
- this.strength=s;
- this.dexterity=d;
- this.intelligence=i;
- this.maxLife=dice.roll()+100;
- this.currentLife=this.maxLife;
- 
-}
 
-public int attack() {
-     int attack=dice.roll()+strength;
-     return attack;
-}
-
-public void wound(int damage) {
-    currentLife=currentLife-damage;
+  
+    for(int i = 1; p1.getCurrentLife() >= 0 && p2.getCurrentLife() >= 0 && i<=3; i++){
+        System.out.println();
+        System.out.println("== Round:" + i+" ==");
+        System.out.println(p1.getName() + ":" + p1.getCurrentLife());
+        System.out.println(p2.getName() + ":" + p2.getCurrentLife());  
+        
+        int a = p1.attack();
+        p2.wound(a);
+        int b = p2.attack();
+        p1.wound(b);
+        int c = p2.castLightningBolt();
+        p2.LightningBolt(c);
+        int d = p2.castHeal();
+        p2.heal(d);
+        int damage1 = p1.attack(), damage2 = p2.castLightningBolt(), heal = p2.castHeal();
+        
+        System.out.println(p1.getName() + " attacks " + p2.getName() + " for " + a);
+        System.out.println(p2.getName() + " attacks " + p1.getName() + " for " + b);
+        System.out.println(p2.getName() + "cast LightningBolt" + " for " + c );
+        System.out.println(p2.getName() + " Healed "+"for " + d);
+       
+       
+        if(p1.getCurrentLife() == p2.getCurrentLife() && i==3){
+            
+            System.out.println(p2.getName() + "'s Health: " + p2.getCurrentLife());
+            System.out.println(p1.getName() + "'s Health: " + p1.getCurrentLife());
     
-}
+            System.out.println("");
+            System.out.println("Draw!");
+            break;
+            
+        }
+        
+        
+        else if(p2.getCurrentLife() < p1.getCurrentLife() && i ==3){
+            System.out.println(p1.getName() + "'s Health: " + p1.getCurrentLife());
+            System.out.println(p2.getName() + "'s Health: " + p2.getCurrentLife());
+            System.out.println(p2.getName() + "cast LightningBolt" + " for " + b );
+            System.out.println("");
+            System.out.println(p1.getName() + " Wins!");
+            break;
+        }
+        
+        else if(p1.getCurrentLife() < p2.getCurrentLife() && i==3){
+            System.out.println(p1.getName() + "'s Health: " + p1.getCurrentLife());
+            System.out.println(p2.getName() + "'s Health: " + p2.getCurrentLife()); 
+            System.out.println("");
+            System.out.println(p2.getName() + " Wins!");
+            break;
+        }
+        
+     
+        
+        }
+    }
 
-public void heal(int heal) {
-     if(currentLife>maxLife)
-         currentLife=currentLife+heal;
-}
-
-public String getName() {
-    return name;
-}
-
-public int getStrength() {
-    return strength;
-}
-
-public int getDexterity() {
-    return dexterity;
-}
-
-public int getIntelligence() {
-    return intelligence;
-}
-
-public int getCurrentLife() {
-    return currentLife;
-}
-
-public int getMaxLife() {
-    return maxLife;
-}
-public void setName(String name){
-        this.name=name;
-}
-public void setStrength(int strength){
-        this.strength=strength;
-}
-public void setDexterity(int dexterity){
-        this.dexterity=dexterity;
-}
-public void setIntelligence(int intelligence){
-        this.intelligence=intelligence;
-}
-public void setMaxLife(int maxLife){
-        this.maxLife=maxLife;
-}
-public void setCurrentLife(int currentLife){
-    this.currentLife=currentLife;
-}
-public int decreaseCurrentLife(int attack){
-    return attack;
-}
-}
+        
+    }
